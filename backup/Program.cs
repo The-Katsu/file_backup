dc455bb8d859189e1d1e4519e831df66ee78badc
unknown <karmaelin8@gmail.com>
2022-04-20 22:40:18 +0300

    get json data & show source files

--- backup/Program.cs
+++ backup/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json.Linq;
+
+var data = GetJsonData("../../../config.json");
+
+ShowSourceFiles(data.root.source);
+
+static void ShowSourceFiles(string source)
+{
+    var files = Directory.GetFileSystemEntries(source, "*", SearchOption.AllDirectories);
+    Console.WriteLine("Cписок файлов и папок в папке источнике:");
+    files.ToList().ForEach(d => Console.WriteLine(d.Replace(source, string.Empty)));
+}
+
+static dynamic GetJsonData(string source)
+{
+    using var sr = new StreamReader(source);
+    var json = sr.ReadToEnd();
+    return JObject.Parse(json);
+}
