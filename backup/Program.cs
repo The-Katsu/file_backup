e9814f40db9ecf55183f55a354bfed04dbf087c7
unknown <karmaelin8@gmail.com>
2022-04-21 20:56:13 +0300

    incremental backup

--- backup/Program.cs
+++ backup/Program.cs
@@ -5,7 +5,62 @@ var data = GetJsonData();
 
 ShowSourceFiles(data.root.source);
 
-FileBackup(data.root.source, data.root.destination);
+Console.WriteLine("Поиск новых файлов или модификаций...");
+
+var sourceFiles = Directory.GetFiles((string)data.root.source, "*.*", SearchOption.AllDirectories);
+
+var destinationFiles = Directory.GetFiles((string)data.root.destination, "*.*", SearchOption.AllDirectories);
+
+List<string> newFiles = sourceFiles.ToList();
+
+foreach (var file1 in sourceFiles)
+{
+    foreach (var file2 in destinationFiles)
+    {
+        if (FileCompare(file1, file2))
+        {
+            newFiles.Remove(file1);
+            break;
+        }
+    }
+}
+
+Console.WriteLine("Найдены файлы:");
+newFiles.ForEach(file => Console.WriteLine(file));
+
+Console.WriteLine("Получаем директории файлов...");
+
+List<string> newFolders = new();
+foreach (var filename in newFiles)
+    newFolders.Add(Path.GetDirectoryName(filename));
+newFolders = newFolders.Distinct().ToList();
+
+Console.WriteLine("Новые директории:");
+newFolders.ForEach(folder => Console.WriteLine(folder));
+
+Console.WriteLine("Создаём новые резервные директории...");
+
+string destinationFolder = "\\base";
+
+if (Directory.GetDirectories((string)data.root.destination).Contains("base"))
+    destinationFolder = string.Format("\\inc_{0:yyyy_MM_dd_HH_mm_ss}", DateTime.Now);
+
+string destinationPath = (string)data.root.destination + destinationFolder;
+
+foreach (var folder in newFolders)
+    Directory.CreateDirectory(folder.Replace((string)data.root.source, destinationPath));
+
+Console.WriteLine("Новые резервные директории:");
+foreach (var folder in Directory.GetDirectories(destinationPath, "*", SearchOption.AllDirectories))
+    Console.WriteLine(folder);
+
+Console.WriteLine("Создаём резервные копии...");
+foreach (var file in newFiles)
+    File.Copy(file, file.Replace((string)data.root.source, destinationPath));
+
+foreach (var file in Directory.GetFiles(destinationPath, "*.*", SearchOption.AllDirectories))
+    Console.WriteLine(file);
+
 
 static void FileBackup(string source, string destination)
 {
